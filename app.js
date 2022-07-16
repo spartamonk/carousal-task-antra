6e1d4a6072f4fc5c9dd40d2a5f7e440d33682ecc
Christopher Akoto <71773056+spartamonk@users.noreply.github.com>
2022-07-16 16:23:54 -0500

    implemented loader

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const rootUrl = 'http://localhost:4232/movies'
 
 const Api = (() => {
   const fetchMovies = () => {
-    return fetch(rootUrl).then((response) => response.json())
+    return fetch(rootUrl).then((response) => {
+      return response.json()
+    })
   }
-
   return {
     fetchMovies,
   }
@@ -17,13 +18,12 @@ const View = (() => {
   const domstr = {
     moviesContainer: '#movies-container',
   }
-
   const render = (ele, tmp) => {
     ele.innerHTML = tmp
   }
-
   const createTmp = (arr) => {
     let tmp = ''
+
     arr.forEach((movie) => {
       tmp += `
         <article class="movie-card">
@@ -61,25 +61,40 @@ const Model = ((api, view) => {
 
   class State {
     #movies = []
+    #loading = true
+    #update = true
 
     get movielist() {
       return this.#movies
     }
     set movielist(movies) {
       this.#movies = [...movies]
-
       const moviesContainer = document.querySelector(
         view.domstr.moviesContainer
       )
-
       const tmp = view.createTmp(this.#movies)
-
       view.render(moviesContainer, tmp)
+
+      this.#loading = false
+    }
+    get loadingstatus() {
+      return this.#loading
+    }
+
+    set loadingstatus(loading) {
+      this.#loading = false
+    }
+
+    get updatestatus() {
+      return this.#update
+    }
+
+    set updatestatus(update) {
+      this.#update = update
     }
   }
 
   const { fetchMovies } = api
-
   return {
     fetchMovies,
     State,
@@ -89,39 +104,59 @@ const Model = ((api, view) => {
 //* ~~~~~~~~~~~~~~~~~~~~~ Controller ~~~~~~~~~~~~~~~~~~~~~
 const Controller = ((model) => {
   const state = new model.State()
+  const slider = document.querySelector('#movies-container')
+  const btnLeft = document.querySelector('.btn-left')
+  const btnRight = document.querySelector('.btn-right')
+  const loader = document.querySelector('.loading')
+  
   // slider
-
   const init = () => {
+    state.loadingstatus = true
     model.fetchMovies().then((movies) => {
       state.movielist = [...movies]
+      state.loadingstatus = false
     })
-    const slider = document.querySelector('#movies-container')
-    const btnLeft = document.querySelector('.btn-left')
-    const btnRight = document.querySelector('.btn-right')
+
+    slider.style.display = 'none'
+    btnLeft.style.display = 'none'
+    btnRight.style.display = 'none'
+    loader.style.display = 'block'
 
     btnLeft.addEventListener('click', () => {
       slider.scrollLeft -= 300
     })
-
     btnRight.addEventListener('click', () => {
       slider.scrollLeft += 300
     })
-
     slider.addEventListener('scroll', () => {
       if (slider.scrollLeft <= 0) {
         btnLeft.classList.add('btn-hidden')
       } else {
         btnLeft.classList.remove('btn-hidden')
-        if (slider.scrollWidth - 1 <= slider.offsetWidth + slider.scrollLeft) {
-          btnRight.classList.add('btn-hidden')
-        } else {
-          btnRight.classList.remove('btn-hidden')
-        }
+      }
+      if (slider.scrollWidth - 1 <= slider.offsetWidth + slider.scrollLeft) {
+        btnRight.classList.add('btn-hidden')
+      } else {
+        btnRight.classList.remove('btn-hidden')
       }
     })
+    // autoplay slider
+  }
+  const disableLoader = () => {
+    if (state.loadingstatus) return
+    slider.style.display = 'flex'
+    btnLeft.style.display = 'inline'
+    btnRight.style.display = 'inline'
+    loader.style.display = 'none'
+
+    state.updatestatus = false
   }
   const bootstrap = () => {
     init()
+
+    while (state.updatestatus) {
+      disableLoader()
+    }
   }
   return { bootstrap }
 })(Model, View)
